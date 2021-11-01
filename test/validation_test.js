ca0bfc2ad63d42a839b51f88be44320ab8daa868
yan <yanhmueaung@gmail.com>
2021-11-01 13:51:21 +0800

    validation done

--- test/validation_test.js
+++ test/validation_test.js
@@ -11,7 +11,22 @@ describe('Validation records', () => {
         assert(message === 'Name is required')
 
     })
-    it('requires a user name', () => {
+    it('requires a user\'s name longer than 2 characters', (done) => {
+        const user = new User({ name: 'Al' });
+        const validation = user.validateSync();
+        const message = validation.errors.name.properties.message;
+        assert(message === 'Name must be longer than 2 characters')
+        done();
+    })
+    it('disallows invalid records from being saved', (done) => {
+        const user = new User({ name: 'Al' });
+        user.save()
+            .catch((valResult) => {
+                console.log(valResult.errors.name.properties.message)
+                const message = valResult.errors.name.properties.message;
+                assert(message === 'Name must be longer than 2 characters')
+                done();
+            })
 
     })
 })
