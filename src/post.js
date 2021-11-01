2c6e4198330dbcce50579ba44d2ea921b91597ff
yan <yanhmueaung@gmail.com>
2021-11-01 14:35:50 +0800

    subdocument create test prepare

---
+++ src/post.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: String
+})
+// const Post=mongoose.model('post',PostSchema);
+module.exports = PostSchema;
